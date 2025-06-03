6a0a605fb2dbe0ca181826a3c47efa8afbc80d6a
andikothari <144172322+andikothari@users.noreply.github.com>
2025-06-03 17:16:43 -0400

    Add movie recommendation view

--- movierecommender/views.py
+++ movierecommender/views.py
@@ -1,6 +1,30 @@
 from . import views
 from .models import Movie
 from django.shortcuts import render
+from django.http import HttpResponseServerError
 
 # HINT: Create a view to provide movie recommendations list for the HTML template
 
+# def movie_recommendation_view(request): 
+#     if request.method == "GET":
+#         # The context/data to be presented in the HTML template
+#         context = {}
+#         # Render a HTML page with specified template and context
+#         return render(request, 'movierecommender/movie_list.html', context)
+
+
+def movie_recommendation_view(request):
+    try:
+        if request.method == "GET":
+            # The context/data to be presented in the HTML template
+            context = {}
+            # Render a HTML page with specified template and context
+            return render(request, 'movierecommender/movie_list.html', context)
+        else:
+            # Handle other HTTP methods if necessary
+            return HttpResponseServerError("Unsupported HTTP method.")
+    except Exception as e:
+        # Log the exception (you can use logging instead of print in a real application)
+        print(f"An error occurred: {e}")
+        # Return a generic error response
+        return HttpResponseServerError("An internal server error occurred.")
--- recommender/urls.py
+++ recommender/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movierecommender/', include('movierecommender.urls'))
-]
+    # Include the URLs from the movierecommender app
+    path('', include('movierecommender.urls')),
+    ]
